95068a33311fb04c2a5c5a06644d9014acfaea81
st609877063@outlook.com <st609877063@outlook.com>
2014-12-29 20:15:42 +0800

    引入struts

---
+++ simple-service-webapp/src/main/java/com/pku/wlj/action/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.pku.wlj.action;
+
+import com.opensymphony.xwork2.Action;
+
+public class HelloWorld implements Action {
+	
+	
+	@Override
+	public String execute() throws Exception {
+		// TODO Auto-generated method stub
+		return "success";
+	}
+
+}
